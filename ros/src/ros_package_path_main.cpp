42d65c746db13697b0bcd610d5a79986053c65f2
rmorales <rmorales@umh.es>
2013-10-01 18:54:42 +0200

    solucionando el directorio ros

---
+++ ros/src/ros_package_path_main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <ros/package.h>
+
+
+
+int  main(int argc,char* argv[])
+{
+
+
+	std::string path = ros::package::getPath("ros_examples_things");
+	// path += "/common/files/stl/"; // Ejemplo de como concatenar path
+	// comentario para el git
+	std::cout << "Path: \n" <<  path <<  std::endl;
+
+	return 0;
+}
