4d6974019881ced2e22dfee24f350f881eb35354
rmorales <rmorales@umh.es>
2013-10-01 19:12:25 +0200

    estoy a medias del ejemplo de stl read

--- ros/src/read_stl_binary_file_main.cpp
+++ ros/src/read_stl_binary_file_main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <ros/package.h>
 
 
 
+// Según:
+// https://code.ros.org/trac/ros-pkg/browser/pkg/trunk/visualization/ogre_tools/src/stl_to_mesh/stl_to_mesh.cpp?rev=11460
 
 int  main(int argc,char* argv[])
 {
 	std::cout << "Probando la lectura del stl binarios" <<  std::endl;
+	std::cout << "------------------------------------" <<  std::endl;
+
+	std::string path = ros::package::getPath("ros_examples_things");
+	path += "/common/files/stl/";
+	std::cout << "Buscando en : \n" <<  path <<  std::endl;
+	std::cout << "el fichero:"  <<  std::endl;
+
+
+
 
 	return 0;
 }
