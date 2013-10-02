3154b0ce99960898ba0983d2f913ebe2aa24d260
rmorales <rmorales@umh.es>
2013-10-02 14:39:45 +0200

    desde el lab

--- ros/src/read_stl_binary_file_main.cpp
+++ ros/src/read_stl_binary_file_main.cpp
@@ -2,6 +2,9 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <ros/package.h>
+#include <stdio.h>
+
+#include <geometric_shapes/shapes.h>
 
 
 
@@ -14,12 +17,79 @@ int  main(int argc,char* argv[])
 	std::cout << "------------------------------------" <<  std::endl;
 
 	std::string path = ros::package::getPath("ros_examples_things");
+	std::string file_name = "big_table.stl";
 	path += "/common/files/stl/";
 	std::cout << "Buscando en : \n" <<  path <<  std::endl;
-	std::cout << "el fichero:"  <<  std::endl;
+	std::cout << "el fichero: "  << file_name <<  std::endl;
+	path += file_name;
+
+	// Open de file
+	FILE* input = fopen(path.c_str(), "r" );
+	if ( !input )
+	{
+		std::cout << "Error no se puede abrir el puto fichero"  <<std::endl;
+		return -1;
+	}
+	std::cout << "Fichero abierto"  <<std::endl;
+
+	fseek( input, 0, SEEK_END );
+	long long fileSize = ftell( input );
+	fseek( input, 0, SEEK_SET );
+	char* buffer = new char[ fileSize ];
+	fread( buffer, fileSize, 1, input );
+	fclose( input );
+
+
+	char* pos = buffer;
+	pos += 80;
+	unsigned int numTriangles = *(unsigned int*)pos;
+	pos += 4;
+	printf( "%d triangles\n", numTriangles );
 
 
 
+
+	unsigned int vertexCount = 0;
+	for ( unsigned int currentTriangle = 0; currentTriangle < numTriangles; ++currentTriangle )
+	{
+		float nX = *(float*)pos;
+		pos += 4;
+		float nY = *(float*)pos;
+		pos += 4;
+		float nZ = *(float*)pos;
+		pos += 4;
+
+		float v1X = *(float*)pos;
+		pos += 4;
+		float v1Y = *(float*)pos;
+		pos += 4;
+		float v1Z = *(float*)pos;
+		pos += 4;
+
+		float v2X = *(float*)pos;
+		pos += 4;
+		float v2Y = *(float*)pos;
+		pos += 4;
+		float v2Z = *(float*)pos;
+		pos += 4;
+
+		float v3X = *(float*)pos;
+		pos += 4;
+		float v3Y = *(float*)pos;
+		pos += 4;
+		float v3Z = *(float*)pos;
+		pos += 4;
+
+		pos += 2;
+
+
+		// Hasta aquí a leido un triangulo (Normal y las 3 coordenadas de los tres vertices)
+		// El siguiente paso es construir el mesh para ROS
+
+		// Buscar moveit_msgs::AttachedCollisionObject
+
+
+	}
 
 	return 0;
 }
